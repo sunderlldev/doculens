78107de5d9a469cc7756d67d820349f1e959f3cb
sunderlldev <juanjoseblas344@gmail.com>
2025-12-22 06:02:34 -0500

    feat(tabBar): show alert when document is created

--- doculens/Modules/MainTabBar/MainTabBarViewController.swift
+++ doculens/Modules/MainTabBar/MainTabBarViewController.swift
@@ -8,31 +8,38 @@
 import UIKit
 
 class MainTabBarViewController: UITabBarController {
-    
+
     private let fabButton = UIButton(type: .system)
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        showLoginIfFirstTime()
         setupFAB()
+        alertaDocumentoCreado()
     }
-    
-    func showLoginIfFirstTime() {
 
-        let alreadyShown = UserDefaults.standard.bool(forKey: "loginModalShown")
-        if alreadyShown { return }
-
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let loginVC = storyboard.instantiateViewController(
-            withIdentifier: "LoginVC"
+    private func alertaDocumentoCreado() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(documentoCreado),
+            name: .documentoCreado,
+            object: nil
         )
-
-        loginVC.modalPresentationStyle = .fullScreen
-        present(loginVC, animated: true)
-
-        UserDefaults.standard.set(true, forKey: "loginModalShown")
     }
 
+    @objc private func documentoCreado() {
+        let alerta = UIAlertController(
+            title: "Documento creado",
+            message: "El documento se escaneo correctamente",
+            preferredStyle: .alert
+        )
+        
+        alerta.addAction(UIAlertAction(title: "Aceptar", style: .default))
+        
+        // Para evitar multiples alertas
+        if presentedViewController == nil {
+            present(alerta, animated: true)
+        }
+    }
 
     private func setupFAB() {
         fabButton.setImage(UIImage(systemName: "plus"), for: .normal)
@@ -40,49 +47,69 @@ class MainTabBarViewController: UITabBarController {
         fabButton.tintColor = .white
         fabButton.layer.cornerRadius = 28
         fabButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        fabButton.addTarget(self, action: #selector(fabButtonTapped), for: .touchUpInside)
-        
+
+        fabButton.addTarget(
+            self,
+            action: #selector(fabButtonTapped),
+            for: .touchUpInside
+        )
+
         view.addSubview(fabButton)
-        
+
         NSLayoutConstraint.activate([
             fabButton.widthAnchor.constraint(equalToConstant: 56),
             fabButton.heightAnchor.constraint(equalToConstant: 56),
-            fabButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24),
-            fabButton.bottomAnchor.constraint(equalTo: tabBar.topAnchor, constant: -16)
+            fabButton.trailingAnchor.constraint(
+                equalTo: view.trailingAnchor,
+                constant: -24
+            ),
+            fabButton.bottomAnchor.constraint(
+                equalTo: tabBar.topAnchor,
+                constant: -16
+            ),
         ])
     }
-    
+
     @objc private func fabButtonTapped() {
         let menu = UIAlertController(
             title: "Nuevo Documento",
             message: nil,
             preferredStyle: .actionSheet
         )
-        
-        menu.addAction(UIAlertAction(
-            title: "Tomar foto",
-            style: .default
-        ) { _ in
-            self.abrirCamara()
-        })
-        
-        menu.addAction(UIAlertAction(
-            title: "Importar PDF",
-            style: .default
-        ) { _ in
-            self.abrirFiles()
-        })
-        
-        menu.addAction(UIAlertAction(
-            title: "Importar Imagen",
-            style: .default
-        ) { _ in
-            self.abrirGaleria()
-        })
-        
+
+        menu.addAction(
+            UIAlertAction(
+                title: "Tomar foto",
+                style: .default
+            ) { _ in
+                self.abrirCamara()
+            }
+        )
+
+        menu.addAction(
+            UIAlertAction(
+                title: "Importar PDF",
+                style: .default
+            ) { _ in
+                self.abrirFiles()
+            }
+        )
+
+        menu.addAction(
+            UIAlertAction(
+                title: "Importar Imagen",
+                style: .default
+            ) { _ in
+                self.abrirGaleria()
+            }
+        )
+
         menu.addAction(UIAlertAction(title: "Cancelar", style: .cancel))
-        
+
         present(menu, animated: true)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
 }
