81eded4c228ed374114b98fe372eae18a90cdc7f
sunderlldev <juanjoseblas344@gmail.com>
2025-12-23 23:09:25 -0500

    fix(app): remove crash lines

--- doculens/Modules/Files/FilesViewController.swift
+++ doculens/Modules/Files/FilesViewController.swift
@@ -54,7 +54,7 @@ class FilesViewController: UIViewController, UICollectionViewDelegate,
             name: .documentoActualizado,
             object: nil
         )
-
+        
         cvDocumentos.delegate = self
         cvDocumentos.dataSource = self
 
